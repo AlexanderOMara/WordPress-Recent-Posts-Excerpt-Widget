bae107beacd954e5bde29ee43b98c2bbd434a2b3
Alexander O'Mara <me@alexomara.com>
2015-04-07 00:57:57 -0400

    Cleanup and read more configurable setting.

--- recentpostsexcerptwidget.php
+++ recentpostsexcerptwidget.php
@@ -3,96 +3,145 @@
 Plugin Name: WordPress Recent Posts Excerpt Widget
 Description: Adds a recent posts widget with excerpts.
 Author: Alexander O'Mara
-Version: 1.7
+Version: 1.8.0
 */
 
 //Extend the widget class and register the widget.
 class RecentPostsExcepts_Widget extends WP_Widget {
 	function RecentPostsExcepts_Widget() {
 		//Create the widget.
-		$this->WP_Widget( 'recentpostexcerpt-widget', 'Recent Post Excerpt Widget', 
+		$this->WP_Widget( 'recentpostexcerpt-widget', __( 'Recent Post Excerpt Widget', 'rpew' ), 
 			//Widget settings.
-			array( 'classname' => 'recentpostexcerptwidget', 'description' => 'Recent Post Excerpt Widget.' ), 
-			//Widget control settings.
+			array( 'classname' => 'recentpostexcerptwidget', 'description' => __( 'Recent Post Excerpt Widget.', 'rpew' ) ), 
 			array( 'id_base' => 'recentpostexcerpt-widget' )
 		);
 	}
 	
-	//Widget display.
 	function widget( $args, $instance ) {
-		
-		extract( $args );
-		$title = isset( $instance['title'] ) ? $instance['title'] : 'Recent Posts';
+		//Get settings.
+		$title       = isset( $instance['title'] )       ? $instance['title']       : __( 'Recent Posts', 'rpew' );
+		$readmore    = isset( $instance['readmore'] )    ? $instance['readmore']    : __( 'Read More...', 'rpew' );
 		$numberposts = isset( $instance['numberposts'] ) ? $instance['numberposts'] : 5;
 		$numberwords = isset( $instance['numberwords'] ) ? $instance['numberwords'] : 100;
-		
+		//Get the date format.
+		$date_format = get_option( 'date_format' );
 		//Widget start wrapper.
-		echo $before_widget;		
+		echo $args['before_widget'];		
 		//Show title between widget title surrounds.
-		echo $before_title . $title . $after_title;
-		
+		echo $args['before_title'], esc_html( $title ), $args['after_title'];
+		//The list of posts.
 		?><ul><?php
-		
-		$recent_posts = wp_get_recent_posts( array(
+		$recent_posts = (array)wp_get_recent_posts( array(
 			'numberposts' => $numberposts,
 			'post_status' => 'publish'
 		) );
-		
 		foreach( $recent_posts as &$p ) {
-			?><li><a href="<?php echo get_permalink( $p['ID'] ); ?>"><?php
-				$author = get_userdata( $p['post_author'] );
-				$postdate = explode( ' ', $p['post_date'] );
-				$excerpt = wp_trim_words( strip_tags( ! empty( $p['post_excerpt'] ) ? $p['post_excerpt'] : str_replace( ']]>', ']]&gt;', apply_filters( 'the_content', strip_shortcodes( $p['post_content'] ) ) ) ), $numberwords );
-				?><h4><?php echo $p['post_title']; ?></h4><?php
-				?><h6><?php echo $author->display_name . ', ' . str_replace( '-', '/', $postdate[0] ); ?></h6><?php
-				?><p><?php echo $excerpt; ?></p><?php
-			?></a></li><?php
+			?><li><?php
+				?><a href="<?php echo get_permalink( $p['ID'] ); ?>"><?php
+					$author = get_userdata( $p['post_author'] );
+					$postdate = apply_filters( 'get_the_date', mysql2date( $date_format, $p['post_date'] ), '', (int)$p['ID'] );
+					$excerpt = trim(
+						wp_trim_words(
+							strip_tags(
+								$p['post_excerpt'] ?
+									$p['post_excerpt'] :
+									str_replace( ']]>', ']]&gt;',
+										apply_filters( 'the_content', strip_shortcodes( $p['post_content'] ) )
+									)
+							)
+						, $numberwords )
+					);
+					?><div class="rpew-title"><?php
+						?><big><?php
+							echo esc_html( $p['post_title'] );
+						?></big><?php
+					?></div><?php
+					?><div class="rpew-meta"><?php
+						?><small><?php
+							echo esc_html( $author->display_name ), _x( ', ', 'author-postdate-sep', 'rpew' ), esc_html( $postdate );
+						?></small><?php
+					?></div><?php
+					?><p class="rpew-excerpt"><?php echo esc_html( $excerpt ); ?></p><?php
+				?></a><?php
+			?></li><?php
 		}
 		unset( $p );
 		?></ul><?php
-		
-		?><a href="<?php echo get_option( 'page_for_posts' ) != '0' ? get_permalink( get_option( 'page_for_posts' ) ) : site_url(); ?>">view all&hellip;</a><?php
-		
+		//The read more link.
+		?><a href="<?php
+			echo (int)get_option( 'page_for_posts' ) !== 0 ? get_permalink( get_option( 'page_for_posts' ) ) : site_url();
+		?>"><?php
+			echo esc_html( $readmore );
+		?></a><?php
 		//Widget end wrapper.
-		echo $after_widget;
+		echo $args['after_widget'];
 	}
 	
-	//Save widget settings.
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		
-		//Save input.
-		$instance['title'] = strip_tags( $new_instance['title'] );
+		//Sanitize settings.
+		$instance['title']       = (string)$new_instance['title'];
+		$instance['readmore']    = (string)$new_instance['readmore'];
 		$instance['numberposts'] = (int)$new_instance['numberposts'];
 		$instance['numberwords'] = (int)$new_instance['numberwords'];
-		
+		//Return settings.
 		return $instance;
 	}
 	
-	//Widget settings.
 	function form( $instance ) {
-		//Default widget settings.
-		$defaults = array(
-			'title' => 'Recent Posts',
+		//PArse settings.
+		$instance = wp_parse_args( (array)$instance, array(
+			'title'       => __( 'Recent Posts', 'rpew' ),
+			'readmore'    => __( 'Read More...', 'rpew' ),
 			'numberposts' => 5,
 			'numberwords' => 100
+		) );
+		//Loop over settings fields.
+		$fields = array(
+			array(
+				'name'  => 'title',
+				'label' => __( 'Title:', 'rpew' ),
+				'value' => $instance['title'],
+				'type'  => 'text'
+			),
+			array(
+				'name'  => 'readmore',
+				'label' => __( 'Read More:', 'rpew' ),
+				'value' => $instance['readmore'],
+				'type'  => 'text'
+			),
+			array(
+				'name'  => 'numberposts',
+				'label' => __( 'Max Posts:', 'rpew' ),
+				'value' => $instance['numberposts'],
+				'type'  => 'number'
+			),
+			array(
+				'name'  => 'numberwords',
+				'label' => __( 'Max words:', 'rpew' ),
+				'value' => $instance['numberwords'],
+				'type'  => 'number'
+			)
 		);
-		$instance = wp_parse_args( (array)$instance, $defaults );
-		?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php echo 'Title:'; ?></label>
-			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" class="widefat" type="text" />
-			<label for="<?php echo $this->get_field_id( 'numberposts' ); ?>"><?php echo 'Number of posts to show:'; ?></label>
-			<input id="<?php echo $this->get_field_id( 'numberposts' ); ?>" name="<?php echo $this->get_field_name( 'numberposts' ); ?>" value="<?php echo $instance['numberposts']; ?>" class="widefat" type="text" />
-			<label for="<?php echo $this->get_field_id( 'numberwords' ); ?>"><?php echo 'Number of words to show:'; ?></label>
-			<input id="<?php echo $this->get_field_id( 'numberwords' ); ?>" name="<?php echo $this->get_field_name( 'numberwords' ); ?>" value="<?php echo $instance['numberwords']; ?>" class="widefat" type="text" />
-		</p>
-		<?php
+		?><p><?php
+			foreach ( $fields as &$field ) {
+				?><label<?php
+					?> for="<?php echo $this->get_field_id( $field['name'] ); ?>"<?php
+				?>><?php
+					echo $field['label'];
+				?></label><?php
+				?><input class="widefat"<?php
+					?> id="<?php echo $this->get_field_id( $field['name'] ); ?>"<?php
+					?> name="<?php echo $this->get_field_name( $field['name'] ); ?>"<?php
+					?> value="<?php echo esc_attr( $field['value'] ); ?>"<?php
+					?> type="<?php echo esc_html( $field['type'] ); ?>"<?php
+				?> /><?php
+			}
+			unset( $field );
+		?></p><?php
 	}
 }
 function recentpostsexcepts_widgets_init() {
 	register_widget( 'RecentPostsExcepts_Widget' );
 }
 add_action( 'widgets_init', 'recentpostsexcepts_widgets_init' );
-
-?>
